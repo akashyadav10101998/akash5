d1fa30fb483d5f7edea868fa3918697ebd12ba4c
akashyadav10101998 <42208424+akashyadav10101998@users.noreply.github.com>
2018-09-02 15:38:10 +0530

    Add files via upload

---
+++ akash5.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int n1,n2;
+cout << "enter first number";
+cin >> n1;
+cout << "enter second number";
+cin >> n2;
+if(n1>n2)
+{
+ cout << "first number" <<n1<< "is largest";
+}
+else
+{
+cout << "second number" <<n2<< "is largest";
+}
+return 0;
+} 
